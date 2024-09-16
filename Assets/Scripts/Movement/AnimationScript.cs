1ea8a60676f0ee64a37d2a5cc3c59fb300300aae
Gerwalter <157617032+Gerwalter@users.noreply.github.com>
2024-09-16 18:23:10 -0300

    Misc animations

--- Assets/Scripts/Movement/AnimationScript.cs
+++ Assets/Scripts/Movement/AnimationScript.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 
 public class AnimationScript : MonoBehaviour
 {
-    public float velocity = 0.0f;
-    public float horizontalVelocity = 0.0f;
-    public float acceleration = 0.0f;
-    public float unacceleration = 0.0f;
     public Animator anim;
     public LayerMask groundLayer;
     [SerializeField] private bool NoAttack = false;
@@ -24,60 +20,9 @@ public class AnimationScript : MonoBehaviour
     private void Update()
     {
         Fight();
-        HandleJump();
-        HandleMovement();
         Cast();
     }
 
-    public void HandleMovement()
-    {
-        float verticalInput = Input.GetAxisRaw("Vertical");
-
-        if (verticalInput > 0.0f)
-        {
-            velocity = 1;
-        }
-        else if (verticalInput < 0.0f)
-        {
-            velocity = -1;
-        }
-        else
-        {
-            velocity = 0;
-        }
-
-        anim.SetFloat("xAxis", velocity);
-        velocity = Mathf.Clamp(velocity, -1.0f, 1.0f);
-
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-
-        if (horizontalInput > 0.0f)
-        {
-            horizontalVelocity = 1;
-        }
-        else if (horizontalInput < 0.0f)
-        {
-            horizontalVelocity = -1;
-        }
-        else
-        {
-            horizontalVelocity = 0;
-        }
-
-        anim.SetFloat("zAxis", horizontalVelocity);
-    }
-
-    private void HandleJump()
-    {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
-
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger("Jump");
-
-        }
-
-    }
     private void Cast()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -88,16 +33,10 @@ public class AnimationScript : MonoBehaviour
     {
         anim.SetTrigger("Cast");
     }
-    public void OnJumpAnimationEnd()
-    {
-        anim.SetTrigger("Jump");
-
-    }
 
     public void SwordReveal()
     {
         sword.SetActive(!sword.activeSelf);
-
     }
 
     [SerializeField] private bool isFighting = false;
@@ -136,7 +75,6 @@ public class AnimationScript : MonoBehaviour
         yield return new WaitForSeconds(attackTimeout);
         AttackFinished();
         yield return new WaitForSeconds(2f);
-
     }
     public void AttackFinished()
     {
