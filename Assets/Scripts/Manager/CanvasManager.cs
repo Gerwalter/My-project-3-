d26c403d8060d4150c1becbf9f07a1988732c350
Gerwalter <157617032+Gerwalter@users.noreply.github.com>
2024-11-26 09:04:51 -0300

    Sceneloading(AhoraSi)

--- Assets/Scripts/Manager/CanvasManager.cs
+++ Assets/Scripts/Manager/CanvasManager.cs
@@ -1,65 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
-    [Header("main menu")]
-    [SerializeField] private Canvas[] _mainMenuScreens;
-
-    private Canvas _activeScene;
-
-    public enum Menu
-    {
-        MainMenu,
-        Options,
-        Credits,
-        // Añade más menús según sea necesario
-    }
-
-    private void Start()
-    {
-        if (_mainMenuScreens.Length > 0)
-        {
-            for (int i = 0; i < _mainMenuScreens.Length; i++)
-            {
-                if (i != 0)
-                {
-                    _mainMenuScreens[i].enabled = false;
-                }
-                else
-                {
-                    _activeScene = _mainMenuScreens[i];
-                }
-            }
-        }
-    }
-
-    // Método para cambiar canvas usando el enum
-    public void ChangeCanvas(Menu menu)
-    {
-        _activeScene.enabled = false;
-        _activeScene = _mainMenuScreens[(int)menu];
-        _activeScene.enabled = true;
-    }
     public void LoadSceneAsync(string scene)
     {
         SceneLoaderManager.Instance.LoadSecenAsync(scene);
     }
 
     // Método para cambiar canvas usando un int
-    public void ChangeCanvas(int index)
-    {
-        if (index < 0 || index >= _mainMenuScreens.Length)
-        {
-            Debug.LogError("Índice de menú fuera de rango");
-            return;
-        }
-        _activeScene.enabled = false;
-        _activeScene = _mainMenuScreens[index];
-        _activeScene.enabled = true;
-    }
 
     public void CloseUp()
     {
