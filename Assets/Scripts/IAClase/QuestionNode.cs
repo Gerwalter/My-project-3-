f7835cd3f780bf3337c74bc63101be45a0c573a2
Gerwalter <157617032+Gerwalter@users.noreply.github.com>
2025-03-19 15:43:36 -0300

    Clase1IA

---
+++ Assets/Scripts/IAClase/QuestionNode.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionNode : Node
+{
+    public Node trueNode;
+    public Node falseNode;
+    public TypeQuestion questionType;
+    public override void Test(Patranger npc)
+    {
+        switch (questionType) 
+        {
+            case TypeQuestion.Steal:
+                if (npc.lupinranger.IsStealing)
+                {
+                    trueNode.Test(npc);
+                }
+                else
+                {
+                    falseNode.Test(npc);
+                }
+                break;
+            case TypeQuestion.Distance:
+                if (Vector3.Distance(npc.lupinranger.transform.position, npc.transform.position) < 2)
+                {
+                    trueNode.Test(npc);
+                }
+                else
+                {
+                    falseNode.Test(npc);
+                }
+                break;
+            case TypeQuestion.Armed:
+                if (npc.lupinranger.IsArmed) 
+                {
+                    trueNode.Test(npc);
+                }
+                else
+                {
+                    falseNode.Test(npc);
+                }
+                break;
+        }
+    }
+}
+
+public enum TypeQuestion
+{
+    Steal, Distance, Armed
+}
