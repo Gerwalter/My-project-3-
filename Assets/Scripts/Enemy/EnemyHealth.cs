b28f9704018f321203f379f6457b01f1553a5330
hellowink <95444380+hellowink@users.noreply.github.com>
2025-10-07 17:08:03 -0300

    Arreglos Build

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -56,6 +56,8 @@ public class EnemyHealth : Health, IEnemy
 
     public void Despawn()
     {
+        Destroy(gameObject);
+        print("AAAAAA");
         gameObject.SetActive(false);
     }
 }
